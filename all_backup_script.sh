c389eb197fa153455ed02bee42ba42f4bb62510c
Shawn Meginley <smjest0802@gmail.com>
2015-11-17 08:59:29 -0500

    Updated to reference new script name.

--- all_backup_script.sh
+++ all_backup_script.sh
@@ -7,9 +7,9 @@ networkDir="/shares/Linux/ubuntu/backups/"
 user="sshd"
 host="10.0.0.3"
 
-./backup_dirs.sh -u ${user} -d ${host} -p family_site_backup_ -l ${localDir} -n ${networkDir} /share/family/
-./backup_dirs.sh -u ${user} -d ${host} -p test_backup_ -l ${localDir} -n ${networkDir} /share/test/
-./backup_dirs.sh -u ${user} -d ${host} -p challenge_backup_ -l ${localDir} -n ${networkDir} /share/challenge/
-./backup_dirs.sh -u ${user} -d ${host} -p utility_backup_ -l ${localDir} -n ${networkDir} /share/utility_scripts/
-./backup_dirs.sh -u ${user} -d ${host} -p shawn_home_backup_ -l ${localDir} -n ${networkDir} /home/shawn/.profile /home/shawn/.vim/ /home/shawn/.vimrc
-./backup_dirs.sh -u ${user} -d ${host} -p html_css_backup_ -l ${localDir} -n ${networkDir} /share/html_css/
+./backup_script.sh -u ${user} -d ${host} -p family_site_backup_ -l ${localDir} -n ${networkDir} /share/family/
+./backup_script.sh -u ${user} -d ${host} -p test_backup_ -l ${localDir} -n ${networkDir} /share/test/
+./backup_script.sh -u ${user} -d ${host} -p challenge_backup_ -l ${localDir} -n ${networkDir} /share/challenge/
+./backup_script.sh -u ${user} -d ${host} -p utility_backup_ -l ${localDir} -n ${networkDir} /share/utility_scripts/
+./backup_script.sh -u ${user} -d ${host} -p shawn_home_backup_ -l ${localDir} -n ${networkDir} /home/shawn/.profile /home/shawn/.vim/ /home/shawn/.vimrc
+./backup_script.sh -u ${user} -d ${host} -p html_css_backup_ -l ${localDir} -n ${networkDir} /share/html_css/
